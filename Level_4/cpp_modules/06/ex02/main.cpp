96e7359f757a91eb12b987b4fde0a298902eed32
MaxBernede <bernede.maximilien@gmail.com>
2024-03-03 22:13:53 +0100

    finished

--- Level_4/cpp_modules/06/ex02/main.cpp
+++ Level_4/cpp_modules/06/ex02/main.cpp
@@ -9,12 +9,54 @@ Base *generate(void);
 void identify(Base* p);
 void identify(Base& p);
 
+void identify(Base* p){
+	//std::cout << "Pointer" << std::endl;
+	if (dynamic_cast<BaseA *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<BaseB *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<BaseC *>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Error with type" << std::endl;
+}
+
+void identify(Base& p){
+	//std::cout << "Reference" << std::endl;
+	try {
+		if (dynamic_cast<BaseA *>(&p))
+		std::cout << "A" << std::endl;
+	}
+	catch (std::bad_cast &error){
+		;
+	}
+	try {
+		if (dynamic_cast<BaseB *>(&p))
+		std::cout << "B" << std::endl;
+	}
+	catch (std::bad_cast &error){
+		;
+	}
+	try {
+		if (dynamic_cast<BaseC *>(&p))
+		std::cout << "C" << std::endl;
+	}
+	catch (std::bad_cast &error){
+		;
+	}
+}
+
 int	main(void)
 {
 	std::srand(static_cast<unsigned int>(std::time(NULL)));
 	// for (int i = 0; i < 20 ; i++){
 	// 	generate();
 	// }
+	Base *base = generate();
+	std::cout << "Identify by pointer: ";
+	identify(base);
+	std::cout << "Identify by address: ";
+	identify(*base);
 	return (0);
 }
 
