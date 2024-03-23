317efe6f7c3c28e613ce08a7ab428c0d915669cf
MaxBernede <bernede.maximilien@gmail.com>
2024-03-23 18:47:46 +0100

    added test copy and ope overload

--- Level_4/cpp_modules/09/ex02/main.cpp
+++ Level_4/cpp_modules/09/ex02/main.cpp
@@ -7,5 +7,19 @@ int main(int ac, char **av) {
 	}
 	PmergeMe pmergeMe(av);
 	
+	{
+		std::cout << "Test copy constructor" << std::endl;
+		PmergeMe pmergeMe2(pmergeMe);
+		pmergeMe2.print_first("Copy: ");
+	}
+	pmergeMe.print_first("Original: ");
+
+	{
+		std::cout << "Test assignation operator" << std::endl;
+		PmergeMe pmergeMe3 = pmergeMe;
+		pmergeMe3.print_first("Assignation: ");
+	}
+	pmergeMe.print_first("Original: ");
+
 	return 0;
 }
